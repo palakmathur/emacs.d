9da6757436bc9519492fc40307c6f2b8d55d2caa
Palak Mathur <palakmathur@Gmail.com>
2019-05-13 18:25:50 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+(setq url-proxy-services
+   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+     ("http" . "proxy:8099")
+     ("https" . "proxy:8099")))
+
+(setq url-http-proxy-basic-auth-storage
+   (list (list "proxy:8099"
+               (cons "Input your LDAP UID !"
+                     (base64-encode-string "UserName:Password")))))
+
+
 ;;;;
 ;; Packages
 ;;;;
@@ -11,9 +22,9 @@
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives
-              '("melpa" . "http://melpa.org/packages/") t)
+              '("melpa" . "http://melpa.org") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org") t)
 
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 (add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
@@ -106,6 +117,9 @@
     ;; project navigation
     projectile
 
+    helm-projectile
+
+
     ;; colorful parenthesis matching
     rainbow-delimiters
 
@@ -155,6 +169,11 @@
 ;; Adding this code will make Emacs enter yaml mode whenever you open
 ;; a .yml file
 (add-to-list 'load-path "~/.emacs.d/vendor")
+
+;;load neotree
+(add-to-list 'load-path "~/.emacs.d/vendor/neotree-20181121.2026")
+(require 'neotree)
+(setq-default neo-show-hidden-files t) ;;show hidden files in neotree
 
 ;;;;
 ;; Customization
