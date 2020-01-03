24374a748af680f128b45822ea1ed55837077cd6
Palak Mathur <palak.mathur@capitalone.com>
2020-01-03 12:29:40 -0600

    Initial Commit - Proxy, Turn off the startup screen and install use-package

---
+++ customizations/setup-proxy-template.el
@@ -0,0 +1,9 @@
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+        ("http" . "proxy:port")
+        ("https" . "proxy:prt")))
+
+(setq url-http-proxy-basic-auth-storage
+      (list (list "proxy:port"
+                  (cons "Input your LDAP UID !"
+                        (base64-encode-string "UserName:Password")))))
---
+++ init.el
@@ -0,0 +1,5 @@
+;;; init.el --- -*- lexical-binding: t -*-,.
+(add-to-list 'load-path "~/.emacs.d/customizations")
+(load "setup-proxy.el")
+
+(org-babel-load-file "~/.emacs.d/README.org")
