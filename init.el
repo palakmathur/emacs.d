5b807b3547704e88abf8cb43ffd35280bebc066b
Palak Mathur <pmathur33@gatech.edu>
2020-09-11 14:14:46 -0500

    Add ido-mode, ido-mode-vertical and iBuffer

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-mode xclip idle-highlight treemacs use-package toc-org projectile org-sidebar org-plus-contrib nova-theme nov magit ivy ereader define-word ample-theme)))
+   '(smex ido-completing-read+ ido-vertical-mode flx-ido lsp-mode xclip idle-highlight treemacs use-package toc-org projectile org-sidebar org-plus-contrib nova-theme nov magit ivy ereader define-word ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
