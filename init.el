8d57220ce11ed301182b1e5f5800fa195b0c1657
Palak Mathur <palakmathur@gmail.com>
2020-09-27 18:03:55 -0500

    Add htmlize for org mode syntax highlighting when exported

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(git-auto-commit-mode org-books org markdown-mode spaceline spaceline-config smex ido-completing-read+ ido-vertical-mode flx-ido lsp-mode xclip idle-highlight treemacs use-package toc-org projectile org-sidebar org-plus-contrib nova-theme nov magit ivy ereader define-word ample-theme)))
+   '(leuven-theme git-auto-commit-mode org-books org markdown-mode spaceline spaceline-config smex ido-completing-read+ ido-vertical-mode flx-ido lsp-mode xclip idle-highlight treemacs use-package toc-org projectile org-sidebar org-plus-contrib nova-theme nov magit ivy ereader define-word ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
