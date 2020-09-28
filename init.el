4887823b5067da7bd520c34b2671de59ecf80211
Palak Mathur <pmathur33@gatech.edu>
2020-09-28 11:28:14 -0500

    Add sbcl and slime

--- init.el
+++ init.el
@@ -15,8 +15,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gac-automatically-push-p nil)
  '(package-selected-packages
-   '(git-auto-commit-mode org-books org markdown-mode spaceline spaceline-config smex ido-completing-read+ ido-vertical-mode flx-ido lsp-mode xclip idle-highlight treemacs use-package toc-org projectile org-sidebar org-plus-contrib nova-theme nov magit ivy ereader define-word ample-theme)))
+   '(slime-autoloads slime git-auto-commit-mode org-books org markdown-mode spaceline spaceline-config smex ido-completing-read+ ido-vertical-mode flx-ido lsp-mode xclip idle-highlight treemacs use-package toc-org projectile org-sidebar org-plus-contrib nova-theme nov magit ivy ereader define-word ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
