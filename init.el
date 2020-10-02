eb6f2bbc48a0483edd1151ffa97e8e43ac8283c8
Palak Mathur <pmathur33@gatech.edu>
2020-10-02 12:29:32 -0500

    Display battery and time in the modeline

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(htmlize yasnippet-snippets xclip which-key use-package treemacs toc-org spacemacs-theme spaceline smex slime projectile org-sidebar org-plus-contrib org-bullets org-books nova-theme nov markdown-mode magit leuven-theme ivy irony-eldoc ido-vertical-mode ido-completing-read+ idle-highlight git-auto-commit-mode flycheck-clang-analyzer flx-ido ereader define-word company-irony company-c-headers ample-theme)))
+   '(doom-modeline all-the-icons htmlize yasnippet-snippets xclip which-key use-package treemacs toc-org spacemacs-theme spaceline smex slime projectile org-sidebar org-plus-contrib org-bullets org-books nova-theme nov markdown-mode magit leuven-theme ivy irony-eldoc ido-vertical-mode ido-completing-read+ idle-highlight git-auto-commit-mode flycheck-clang-analyzer flx-ido ereader define-word company-irony company-c-headers ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
