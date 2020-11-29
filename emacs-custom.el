483411e3dae7a93eebe269b7d5f6433a9de5c857
Palak <pmathur33@gatech.edu>
2020-11-28 20:34:19 -0600

    Fix merge issues

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,17 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-<<<<<<< HEAD
    '("6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default))
  '(helm-completion-style 'emacs)
  '(package-selected-packages
    '(helpful company flycheck yasnippet markdown-mode projectile magit helm all-the-icons tide js2-mode lsp-java dap-mode lsp-ui lsp-mode dumb-jump racer cargo flycheck-rust rust-mode yasnippet-snippets yaml-mode xclip which-key visual-fill-column use-package try treemacs toc-org spacemacs-theme spaceline smex slime rainbow-delimiters org-sidebar org-plus-contrib org-bullets org-books nova-theme nov leuven-theme ivy irony-eldoc ido-vertical-mode ido-completing-read+ idle-highlight htmlize helm-rg helm-projectile git-auto-commit-mode forge flycheck-clang-analyzer flx-ido ereader doom-themes doom-modeline define-word company-irony company-c-headers ample-theme))
-=======
-   '("bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" default))
  '(helm-completion-style 'emacs)
  '(package-selected-packages
    '(ob-kotlin pdf-tools know-your-http-well magit-todos company flycheck yasnippet markdown-mode projectile magit helm all-the-icons tide js2-mode lsp-java dap-mode lsp-ui lsp-mode dumb-jump racer cargo flycheck-rust rust-mode yasnippet-snippets yaml-mode xclip which-key visual-fill-column use-package try treemacs toc-org spacemacs-theme spaceline smex slime rainbow-delimiters org-sidebar org-plus-contrib org-bullets org-books nova-theme nov leuven-theme ivy irony-eldoc ido-vertical-mode ido-completing-read+ idle-highlight htmlize helm-rg helm-projectile git-auto-commit-mode forge flycheck-clang-analyzer flx-ido ereader doom-themes doom-modeline define-word company-irony company-c-headers ample-theme))
->>>>>>> 9651f65b7a7f3eb3d459471dfcfae25e142aa8b2
  '(safe-local-variable-values
    '((gac-default-message . "Update the worklist")
      (gac-debounce-interval . 120)
